a385bb3992d05f359f785e97f71aef035cfe8a90
Mikhail Zagurskiy <murzagurskiy@gmail.com>
2024-01-01 11:38:51 +0200

    Introduce HandlerStatus

--- handlers/common.py
+++ handlers/common.py
@@ -5,6 +5,40 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
 from aiogram.types import Message, ReplyKeyboardRemove
 
+from enum import Enum
+
+
+class Status(Enum):
+  UNDEFINED = 0
+  FAILED = 1
+  IN_PROGRESS = 2
+  DONE = 3
+
+
+class HandlerStatus:
+  def __init__(self, status: Status, handler_name: str):
+    self.status = status
+    self.name = handler_name
+
+  def __repr__(self):
+    return f'{self.name} is {self.status}'
+
+  def failed(handler_name: str):
+    return HandlerStatus(Status.FAILED, handler_name)
+
+  def in_progress(handler_name: str):
+    return HandlerStatus(Status.IN_PROGRESS, handler_name)
+
+  def done(handler_name: str):
+    return HandlerStatus(Status.DONE, handler_name)
+
+  def is_failed(self):
+    return self.status == Status.FAILED
+
+  def is_done(self):
+    return self.status == Status.DONE
+
+
 router = Router()
 
 
